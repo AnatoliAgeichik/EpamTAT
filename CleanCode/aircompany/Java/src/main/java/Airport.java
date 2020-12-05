40736abe0f51d71dabaa0bf90386a7a6c29a4aca
AnatoliAgeichik <3anatoliageichik@gmail.com>
2020-12-05 19:44:35 +0300

    fixed airport and plane classes

---
+++ CleanCode/aircompany/Java/src/main/java/Airport.java
@@ -0,0 +1,90 @@
+import Planes.ExperimentalPlane;
+import models.MilitaryType;
+import Planes.MilitaryPlane;
+import Planes.PassengerPlane;
+import Planes.Plane;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Airport {
+    private List<? extends Plane> planes;
+
+
+
+    public List<PassengerPlane> getPassengerPlanes() {
+        return planes.stream()
+                .filter(plane -> plane instanceof PassengerPlane)
+                .map(plane -> (PassengerPlane) plane)
+                .collect(Collectors.toList());
+    }
+
+    public List<MilitaryPlane> getMilitaryPlanes() {
+        return planes.stream()
+                .filter(plane -> plane instanceof MilitaryPlane)
+                .map(plane -> (MilitaryPlane) plane)
+                .collect(Collectors.toList());
+    }
+
+    public PassengerPlane getPassengerPlaneWithMaxPassengersCapacity() {
+        return getPassengerPlanes().stream().max(Comparator.comparing(PassengerPlane::getPassengersCapacity)).get();
+    }
+
+
+    public List<MilitaryPlane> getTransportMilitaryPlanes() {
+        return getMilitaryPlanes().stream()
+                .filter(militaryPlane -> militaryPlane.getType().equals(MilitaryType.TRANSPORT))
+                .collect(Collectors.toList());
+    }
+
+    public List<MilitaryPlane> getBomberMilitaryPlanes() {
+        return getMilitaryPlanes().stream()
+                .filter(militaryPlane -> militaryPlane.getType().equals(MilitaryType.BOMBER))
+                .collect(Collectors.toList());
+    }
+
+    public List<ExperimentalPlane> getExperimentalPlanes() {
+        return planes.stream()
+                .filter(plane -> plane instanceof ExperimentalPlane)
+                .map(plane -> (ExperimentalPlane) plane)
+                .collect(Collectors.toList());
+    }
+
+    public Airport sortByMaxDistance() {
+        planes.sort(Comparator.comparingInt(Plane::getMaxFlightDistance));
+        return this;
+    }
+
+    public Airport sortByMaxSpeed() {
+        planes.sort(Comparator.comparingInt(Plane::getMaxSpeed));
+        return this;
+    }
+
+    public Airport sortByMaxLoadCapacity() {
+        planes.sort(Comparator.comparingInt(Plane::getMinLoadCapacity));
+        return this;
+    }
+
+    public List<? extends Plane> getPlanes() {
+        return planes;
+    }
+
+    private void print() {
+        for(Plane plane : planes){
+                System.out.println(plane);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Airport{" +
+                "Planes=" + planes.toString() +
+                '}';
+    }
+
+
+    public Airport(List<? extends Plane> planes) {
+        this.planes = planes;
+    }
+
+}
