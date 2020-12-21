52d5b8e31ba38b867e3e1dc6392c82ba894222d2
AnatoliAgeichik <3anatoliageichik@gmail.com>
2020-12-21 23:35:24 +0300

    fixed bugs

--- FrameworkLab/src/test/java/com/epam/tat/util/IntegerUtil.java
+++ FrameworkLab/src/test/java/com/epam/tat/util/IntegerUtil.java
@@ -2,7 +2,7 @@ package com.epam.tat.util;
 
 public class IntegerUtil {
     public static int parsePrice(String priceStr){
-           
+
         return Integer.parseInt(priceStr.replaceAll("[^\\d]", ""));
     }
 }
